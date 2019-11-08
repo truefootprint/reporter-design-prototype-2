9a71cae11279b82d5904ec04544dede60cd66d4f
Chris Patuzzo <chris@patuzzo.co.uk>
2019-11-08 10:33:28 +0000

    Add mobile app headers

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,11 +1,19 @@
+import Head from "next/head";
 import css from "./styles.scss";
 
 import "./reset.scss";
 
-const Layout = ({ children }) => (
+const Layout = ({ children }) => <>
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+    <meta name="mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+  </Head>
+
   <div className={css.layout}>
     {children}
   </div>
-);
+</>;
 
 export default Layout;
