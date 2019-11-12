6598f4ca7343abc63ab520d2c13421814be90df9
Chris Patuzzo <chris@patuzzo.co.uk>
2019-11-12 09:50:58 +0000

    Fix the numbering

--- pages/garden_path.js
+++ pages/garden_path.js
@@ -63,7 +63,7 @@ const GardenPath = () => {
         <p>A common problem with builders laying foundations is that they use inferior quality cement. Sometimes they even don't use any cement and just use sand. In the long run this results in uneven paths. Another common issue is skimping on materials and making the foundation too short or too narrow.</p>
 
         <Card>
-          <span className="ordinal">1 of 2</span>
+          <span className="ordinal">1 of 7</span>
           <h3>Cement</h3>
           <p>Does the cement float in water? </p>
           <MultiChoice choices={["Yes", "No", "Not sure"]} />
@@ -71,7 +71,7 @@ const GardenPath = () => {
         </Card>
 
         <Card>
-          <span className="ordinal">2 of 2</span>
+          <span className="ordinal">2 of 7</span>
           <h3>Cement</h3>
           <p>Does it contain lumps?</p>
           <MultiChoice choices={["Yes", "No", "Not sure"]} />
@@ -79,7 +79,7 @@ const GardenPath = () => {
         </Card>
 
         <Card>
-          <span className="ordinal">1 of 5</span>
+          <span className="ordinal">3 of 7</span>
           <h3>Sizes and position</h3>
           <p>What is the width of the foundation?</p>
           <ExpectedValue>It should be 92 centimeters wide</ExpectedValue>
@@ -89,7 +89,7 @@ const GardenPath = () => {
         </Card>
 
         <Card>
-          <span className="ordinal">2 of 5</span>
+          <span className="ordinal">4 of 7</span>
           <h3>Sizes and position</h3>
           <p>What is the length of the foundation?</p>
           <ExpectedValue>It should be 212 centimeters long</ExpectedValue>
@@ -98,7 +98,7 @@ const GardenPath = () => {
         </Card>
 
         <Card>
-          <span className="ordinal">3 of 5</span>
+          <span className="ordinal">5 of 7</span>
           <h3>Sizes and position</h3>
           <p>Is the foundation going from gate to doorstep?</p>
           <MultiChoice choices={["Yes", "No", "Not sure"]} />
@@ -106,7 +106,7 @@ const GardenPath = () => {
         </Card>
 
         <Card>
-          <span className="ordinal">4 of 5</span>
+          <span className="ordinal">6 of 7</span>
           <h3>Sizes and position</h3>
           <p>Is the surface smooth?</p>
           <MultiChoice choices={["Yes", "No", "Not sure"]} />
@@ -114,7 +114,7 @@ const GardenPath = () => {
         </Card>
 
         <Card>
-          <span className="ordinal">5 of 5</span>
+          <span className="ordinal">7 of 7</span>
           <h3>Sizes and position</h3>
           <p>Has this activity been completed?</p>
           <MultiChoice choices={["Yes", "No", "Not sure"]} />
@@ -126,7 +126,7 @@ const GardenPath = () => {
         <p>Key things to look out for during this phase is whether they are using the right tiles, how level the tiles are, and whether they cover the foundation entirely.</p>
 
         <Card>
-          <span className="ordinal">1 of 4</span>
+          <span className="ordinal">1 of 10</span>
           <h3>Tiles</h3>
           <p>Are the tiles black and white?</p>
           <MultiChoice choices={["Yes", "No", "Not sure"]} />
@@ -134,7 +134,7 @@ const GardenPath = () => {
         </Card>
 
         <Card>
-          <span className="ordinal">2 of 4</span>
+          <span className="ordinal">2 of 10</span>
           <h3>Tiles</h3>
           <p>What is the width of the biggest tile?</p>
           <FreeText placeholder="Add a value" unit="cm" />
@@ -142,7 +142,7 @@ const GardenPath = () => {
         </Card>
 
         <Card>
-          <span className="ordinal">3 of 4</span>
+          <span className="ordinal">3 of 10</span>
           <h3>Tiles</h3>
           <p>What is the length of the biggest tile?</p>
           <FreeText placeholder="Add a value" unit="cm" />
@@ -150,7 +150,7 @@ const GardenPath = () => {
         </Card>
 
         <Card>
-          <span className="ordinal">4 of 4</span>
+          <span className="ordinal">4 of 10</span>
           <h3>Tiles</h3>
           <p>Are there any broken tiles?</p>
           <MultiChoice choices={["Yes", "No", "Not sure"]} />
@@ -158,7 +158,7 @@ const GardenPath = () => {
         </Card>
 
         <Card>
-          <span className="ordinal">1 of 3</span>
+          <span className="ordinal">5 of 10</span>
           <h3>Laying them</h3>
           <p>Have the tiles been laid in a mosaic pattern?</p>
           <MultiChoice choices={["Yes", "No", "Not sure"]} />
@@ -166,7 +166,7 @@ const GardenPath = () => {
         </Card>
 
         <Card>
-          <span className="ordinal">2 of 3</span>
+          <span className="ordinal">6 of 10</span>
           <h3>Laying them</h3>
           <p>Do the tiles cover the foundation entirely?</p>
           <MultiChoice choices={["Yes", "No", "Not sure"]} />
@@ -174,7 +174,7 @@ const GardenPath = () => {
         </Card>
 
         <Card>
-          <span className="ordinal">3 of 3</span>
+          <span className="ordinal">7 of 10</span>
           <h3>Laying them</h3>
           <p>Is the surface even?</p>
           <MultiChoice choices={["Yes", "No", "Not sure"]} />
@@ -182,7 +182,7 @@ const GardenPath = () => {
         </Card>
 
         <Card>
-          <span className="ordinal">1 of 3</span>
+          <span className="ordinal">8 of 10</span>
           <h3>Tidy-up</h3>
           <p>Have all the remaining tiles been handed over?</p>
           <MultiChoice choices={["Yes", "No", "Not sure"]} />
@@ -190,7 +190,7 @@ const GardenPath = () => {
         </Card>
 
         <Card>
-          <span className="ordinal">2 of 3</span>
+          <span className="ordinal">9 of 10</span>
           <h3>Tidy-up</h3>
           <p>Have all the remaining items been removed from the premises?</p>
           <MultiChoice choices={["Yes", "No", "Not sure"]} />
@@ -198,7 +198,7 @@ const GardenPath = () => {
         </Card>
 
         <Card>
-          <span className="ordinal">3 of 3</span>
+          <span className="ordinal">10 of 10</span>
           <h3>Tidy-up</h3>
           <p>Has this activity been completed?</p>
           <MultiChoice choices={["Yes", "No", "Not sure"]} />
